5dd0adb1ce743d811476d029629dc55dcb9374da
Andy Nguyen <andyn4674@gmail.com>
2025-08-20 17:11:03 -0500

    Initial Files

---
+++ src/test/java/com/andyn4674/SpringSecurityWithDatabase/SpringSecurityWithDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andyn4674.SpringSecurityWithDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
