836c8c235e21d30db3ad0a89e7c3b36042503525
Andy Nguyen <andyn4674@gmail.com>
2025-12-17 00:52:38 -0600

    JWT added

--- src/main/java/com/andyn4674/SpringSecurityWithDatabase/config/SecurityConfig.java
+++ src/main/java/com/andyn4674/SpringSecurityWithDatabase/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.andyn4674.SpringSecurityWithDatabase.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -23,32 +26,41 @@ public class SecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
         return http.csrf(customizer -> customizer.disable()) // removes csrf token requirement
-            .authorizeHttpRequests(request -> request.anyRequest().authenticated()) // requires all attempts to access be authenticated
+            .authorizeHttpRequests(request -> 
+                request.requestMatchers("register", "login").
+                permitAll().
+                anyRequest().
+                authenticated()) // requires all attempts to access be authenticated
             // .formLogin(Customizer.withDefaults()) // form sign in
             .httpBasic(Customizer.withDefaults())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // removes session id
+            .sessionManagement(session -> 
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // removes session id
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService()
-    {
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-            .username("username")
-            .password("password")
-            .roles("ADMIN")
-            .build();
-        UserDetails user2 = User.withDefaultPasswordEncoder()
-            .username("user2")
-            .password("pass2")
-            .roles("USER")
-            .build();
-        return new InMemoryUserDetailsManager(user1, user2);
-    }
+    // @Bean
+    // public UserDetailsService userDetailsService()
+    // {
+    //     UserDetails user1 = User.withDefaultPasswordEncoder()
+    //         .username("username")
+    //         .password("password")
+    //         .roles("ADMIN")
+    //         .build();
+    //     UserDetails user2 = User.withDefaultPasswordEncoder()
+    //         .username("user2")
+    //         .password("pass2")
+    //         .roles("USER")
+    //         .build();
+    //     return new InMemoryUserDetailsManager(user1, user2);
+    // }
 
     @Bean
     public AuthenticationProvider authenticationProvider()
@@ -57,5 +69,11 @@ public class SecurityConfig {
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         provider.setUserDetailsService(userDetailsService);
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception
+    {
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/com/andyn4674/SpringSecurityWithDatabase/controller/UserController.java
+++ src/main/java/com/andyn4674/SpringSecurityWithDatabase/controller/UserController.java
@@ -20,4 +20,9 @@ public class UserController {
         return service.register(user);
     }
 
+    @PostMapping("/login")
+    public String login(@RequestBody Users user)
+    {
+        return service.verify(user);
+    }
 }
